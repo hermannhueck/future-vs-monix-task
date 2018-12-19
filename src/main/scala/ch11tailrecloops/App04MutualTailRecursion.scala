3c98e32d4379257819e9b6b231e561906e578c09
Hermann Hueck <hermann.hueck@mailbox.org>
2018-12-19 18:08:44 +0100

    + tail recursive loops

---
+++ src/main/scala/ch11tailrecloops/App04MutualTailRecursion.scala
@@ -0,0 +1,30 @@
+package ch11tailrecloops
+
+import monix.eval.Task
+import monix.execution.Scheduler
+
+object App04MutualTailRecursion extends App {
+
+  println("\n-----")
+
+  def odd(n: Int): Task[Boolean] =
+    Task.eval(n == 0).flatMap {
+      case true => Task.now(false)
+      case false => if (n > 0) even(n - 1) else even(n + 1)
+    }
+
+  def even(n: Int): Task[Boolean] =
+    Task.eval(n == 0).flatMap {
+      case true => Task.now(true)
+      case false => if (n > 0) odd(n - 1) else odd(n + 1)
+    }
+
+  val task: Task[Boolean] = even(-1000000)
+
+  implicit val scheduler: Scheduler = Scheduler.global
+
+  task foreach println
+
+  Thread.sleep(1000L)
+  println("-----\n")
+}
